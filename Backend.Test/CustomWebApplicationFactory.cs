454fcd8af1917389930f3ef7e9812caf68f8364c
Noah Hood <hood.noah@icloud.com>
2023-08-04 11:26:47 -0400

    add per-method test isolation

--- Backend.Test/CustomWebApplicationFactory.cs
+++ Backend.Test/CustomWebApplicationFactory.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
-public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>, IDisposable
 {
   protected override void ConfigureWebHost(IWebHostBuilder builder)
   {
@@ -23,11 +23,57 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
         services.Remove(descriptor);
       }
 
+      // Add dbContext
       services.AddDbContext<RecipeContext>(Options =>
       {
         Options.UseNpgsql("Host=localhost;Port=5432;Database=recipes_test;Username=test_user;Password=test_password");
       });
+
+      // Create an intermediate service provider
+      var serviceProvider = services.BuildServiceProvider();
+
+      // Create a scope to get reference to the dbContext
+      using var scope = serviceProvider.CreateScope();
+      var scopedService = scope.ServiceProvider;
+      var db = scopedService.GetRequiredService<RecipeContext>();
+
+      // Ensure db is created
+      db.Database.EnsureCreated();
+
+      // Run migrations
+      try
+      {
+        db.Database.Migrate();
+      }
+      catch (Exception)
+      {
+        Console.Error.WriteLine("Failed to migrate database");
+      }
     });
+  }
+
+  /// <summary>
+  /// Cleans up the database by deleting all records.
+  /// Does not delete the database itself since the schema
+  /// should not change between uses.
+  /// </summary>
+  public void DeleteAllRecords()
+  {
+    using var scope = Services.CreateScope();
+    var scopedService = scope.ServiceProvider;
+    var db = scopedService.GetRequiredService<RecipeContext>();
+
+    db.Recipes.RemoveRange(db.Recipes);
+    db.SaveChanges();
+  }
+
+  public void CleanUp()
+  {
+    using var scope = Services.CreateScope();
+    var scopedService = scope.ServiceProvider;
+    var db = scopedService.GetRequiredService<RecipeContext>();
+
+    db.Database.EnsureDeleted();
   }
 }
 
--- Backend.Test/GetRecipesTest.cs
+++ Backend.Test/GetRecipesTest.cs
@@ -1,18 +1,49 @@
+using System.Net.Http.Json;
+
 namespace Backend.IntegrationTests;
 
-public class GetRecipesIntegrationTests : IClassFixture<CustomWebApplicationFactory>
+public class GetRecipesIntegrationTests : IClassFixture<CustomWebApplicationFactory>, IDisposable
 {
   private readonly HttpClient _client;
+  private readonly CustomWebApplicationFactory _factory;
 
   public GetRecipesIntegrationTests(CustomWebApplicationFactory factory)
   {
+    _factory = factory;
     _client = factory.CreateClient();
   }
+
+  // Clean up the database after each test
+  public void Dispose()
+  {
+    _factory.DeleteAllRecords();
+  }
+
 
   [Fact]
   public async Task GetRecipes_ReturnsSuccessStatusCode()
   {
     var response = await _client.GetAsync("/recipes");
     response.EnsureSuccessStatusCode();
+  }
+
+  [Fact]
+  public async Task GetRecipes_Always_ReturnsEmptyListWhenNoRecipes()
+  {
+    var response = await _client.GetAsync("/recipes");
+    var recipes = await response.Content.ReadFromJsonAsync<List<Recipe>>();
+    Assert.NotNull(recipes);
+    Assert.Empty(recipes);
+  }
+
+  [Fact]
+  public async Task PostRecipe_Always_ReturnsCreatedRecipe()
+  {
+    var recipe = new Recipe { Name = "Test Recipe" };
+    var response = await _client.PostAsJsonAsync("/recipes", recipe);
+    response.EnsureSuccessStatusCode();
+    var createdRecipe = await response.Content.ReadFromJsonAsync<Recipe>();
+    Assert.NotNull(createdRecipe);
+    Assert.Equal(recipe.Name, createdRecipe.Name);
   }
 }
